f6944aa0456566baeff27af5167a7a9ae7b1f6c4
Huzaifa Ali Khan <azmathuzaifa@gmail.com>
2024-11-11 09:09:13 +0500

    slight changes

--- rowMajor.cpp
+++ rowMajor.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n = 3;
+    int n = 4;
     int I[n] = {1,2,3,4}; // Example initialization for I
     int S[n] = {5,5,5,5}; // Example initialization for S
     int sum = 0;
